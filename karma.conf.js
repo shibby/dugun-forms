7000617eaee66768f0a00aa8239551c328ef823a
Ege Sertçetin <ege@sertcetin.com>
2016-05-11 14:56:34 +0300

    Added ng-html2js-preprocessor to karma

--- karma.conf.js
+++ karma.conf.js
@@ -12,13 +12,27 @@ module.exports = function(config) {
         files: [
             {pattern: 'src/module.js'},
             {pattern: 'src/**/*.js'},
+            {pattern: 'src/**/*.html'},
             {pattern: 'test/**/*.js'}
         ],
+
+        preprocessors: {
+            '**/*.html': ['ng-html2js']
+        },
+
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'src/',
+            cacheIdFromPath: function(filepath) {
+                var cacheId = filepath.replace('src/', '');
+                return cacheId;
+            },
+            moduleName: 'dugun.forms'
+        },
 
         autoWatch : true,
 
         browsers: [
             'Chrome',
-        ],
+        ]
     });
 };
