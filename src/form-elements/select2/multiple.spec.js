0d6b92a053a124f8b582db260c8aede79f65f6a5
Ege Sertçetin <ege@sertcetin.com>
2016-05-11 14:57:45 +0300

    Test for dgFormSelect2 and dgFormSelect2Multiple directives

---
+++ src/form-elements/select2/multiple.spec.js
@@ -0,0 +1,17 @@
+describe('dgFormSelect2Multiple directive', function() {
+    var $compile,
+        $rootScope;
+
+    beforeEach(module('dugun.forms'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_){
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('compiles the code', (function() {
+        var element = $compile('<dg-form-select2-multiple options="[{id: 1, name: \'a\'}]"></dg-form-select2-multiple>')($rootScope);
+        $rootScope.$digest();
+        expect($(element).find('.ui-select-container.select2.ui-select-multiple').length).toBeTruthy();
+    }));
+});
---
+++ src/form-elements/select2/single.spec.js
@@ -0,0 +1,17 @@
+describe('dgFormSelect2 directive', function() {
+    var $compile,
+        $rootScope;
+
+    beforeEach(module('dugun.forms'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_){
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('compiles the code', (function() {
+        var element = $compile('<dg-form-select2 options="[{id: 1, name: \'a\'}]"></dg-form-select2>')($rootScope);
+        $rootScope.$digest();
+        expect($(element).find('.ui-select-container.select2').length).toBeTruthy();
+    }));
+});
