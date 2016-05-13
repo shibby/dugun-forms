a165f9bf62cf2925a08c098dc05513254b65e5ec
Ege Sertçetin <ege@sertcetin.com>
2016-05-13 16:53:06 +0300

    Added further tests for dgFormDate

--- src/form-elements/date/date.spec.js
+++ src/form-elements/date/date.spec.js
@@ -16,4 +16,23 @@ describe('dgFormDate directive', function() {
         $rootScope.$digest();
         expect($(element).find('input').attr('uib-datepicker-popup')).toBeDefined();
     });
+
+    it('when model changed, view value is set', function() {
+        $rootScope.dateModel = null;
+        var element = $compile(
+            '<form name="form">' +
+            '<dg-form-date ng-model="dateModel" name="dateField"></dg-form-date>' +
+            '</form>'
+        )($rootScope);
+        $rootScope.$digest();
+        var form = $rootScope.form;
+        var isolatedScope = element.find('dg-form-date').isolateScope();
+        expect(form.dateField.$viewValue).toBe(null);
+
+        $rootScope.dateModel = '2015-01-01';
+        $rootScope.$digest();
+        expect(form.dateField.$viewValue).toBe('2015-01-01');
+
+        expect(isolatedScope.date.constructor.name).toBe('Date');
+    });
 });
