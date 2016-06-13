9e358db7312e66722f010587f5d1f10c26f1bc9f
Ege Sertçetin <ege@sertcetin.com>
2016-06-13 17:57:05 +0300

    Added test for displaying options

--- src/form-elements/select2/single.spec.js
+++ src/form-elements/select2/single.spec.js
@@ -14,4 +14,22 @@ describe('dgFormSelect2 directive', function() {
         $rootScope.$digest();
         expect($(element).find('.ui-select-container').length).toBeTruthy();
     }));
+
+    it('displays options', function() {
+        $rootScope.options = [
+            {id: 1, name: 'first'},
+            {id: 2, name: 'second'},
+        ];
+
+        var element = $compile('<dg-form-select2 options="options" ng-model="model"></dg-form-select2>')($rootScope);
+        $rootScope.$digest();
+
+        var uiSelectScope = element.find('.ui-select-container').scope();
+        uiSelectScope.$select.open = true;
+        uiSelectScope.$digest();
+
+        expect($(element).find('.ui-select-choices-row-inner p').first().text()).toBe('first');
+
+        console.log($(element).find('.ui-select-choices-row-inner p').first().text());
+    });
 });
