b2cc5e56c623f0c7a5366e4348639527353d8649
Ege Sertçetin <ege@sertcetin.com>
2016-06-14 11:43:00 +0300

    Test for options and placeholder

--- src/form-elements/select2/multiple.spec.js
+++ src/form-elements/select2/multiple.spec.js
@@ -14,4 +14,34 @@ describe('dgFormSelect2Multiple directive', function() {
         $rootScope.$digest();
         expect($(element).find('.ui-select-container.ui-select-multiple').length).toBeTruthy();
     }));
+
+    it('displays options', function() {
+        $rootScope.options = [
+            {id: 1, name: 'first'},
+            {id: 2, name: 'second'},
+        ];
+
+        var element = $compile('<dg-form-select2-multiple options="options" ng-model="model"></dg-form-select2-multiple>')($rootScope);
+        $rootScope.$digest();
+
+        var uiSelectScope = element.find('.ui-select-container').scope();
+        uiSelectScope.$select.open = true;
+        uiSelectScope.$digest();
+
+        expect($(element).find('.ui-select-choices-row-inner p').first().text()).toBe('first');
+    });
+
+    it('displays placeholder', function () {
+        $rootScope.options = [
+            {id: 1, name: 'first'},
+            {id: 2, name: 'second'},
+        ];
+
+        var element = $compile('<dg-form-select2-multiple placeholder="Nice multiselect" options="options" ng-model="model"></dg-form-select2-multiple>')($rootScope);
+        $rootScope.$digest();
+
+        var placeholderElement = element.find('.ui-select-match').first();
+
+        expect(placeholderElement.attr('placeholder')).toBe('Nice multiselect');
+    });
 });
